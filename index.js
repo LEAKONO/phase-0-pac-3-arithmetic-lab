cc4b8f8c56d217aca64f36b074c43eecdc4fc740
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-03-14 02:33:17 +0300

    Arithmetic lab

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b){
+return a+b
+}
+function subtract(a,b){
+return a-b
+}
+function multiply(a,b){
+return a*b
+}
+function divide(a,b){
+return a/b
+}
+function increment(n) {
+    return n + 1;
+}
+function decrement(n){
+    return n-1
+}
+function makeInt(n){
+    return n
+    return ( 0*2328)
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function  preserveDecimal(n){
+
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+    }
+
+
